7780bbd36bfa0d1a73ae2ce50ec4c78a4ad8b8a4
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2022-10-18 23:54:37 +0200

    Update code, PDF; Added Linear regression line, need to double check

--- scatter.jl
+++ scatter.jl
@@ -3,29 +3,39 @@ using Dates
 using GLM
 using DataFrames
 
+function get_beta_coefficent(x::Vector{Date}, y::Vector{Float64})::Float64 
+    Date
+end
+
+df = DataFrame(x = [2018.0, 2019.09, 2019.71, 2020.12, 2020.44, 2021.77])
+
+y = [0.0936, 1.5, 8.3, 17, 175, 530]
+df.y = map(log10, y)
+scatter(df.x, df.y, label="Data points")
+model = lm(@formula(y ~ x), df)  # Note how the formula looks exactly like the model you want to build
+plot!(df.x, predict(model, df), label="Linear regression")
+
+#=
 # Years from data
-x_1 = [Date(2018), Date(2019, 2, 14), Date(2019, 9, 17), Date(2020, 2, 13), Date(2020, 6, 11), Date(2021, 10, 11)]
-x = [2018, 2019, 2019, 2020, 2020, 2021]
+x = [Date(2018), Date(2019, 2, 14), Date(2019, 9, 17), Date(2020, 2, 13), Date(2020, 6, 11), Date(2021, 10, 11)]
 
 # Number of parameters in billions, from data
 y = [0.0936, 1.5, 8.3, 17, 175, 530]
 
 # Y_i = log10(X_i)
 y_1 = map(log10, y)
+x_t = map(Dates.DateTime, x)
+x_1 = map(Dates.datetime2unix, x_t)
+x_2 = [2018, 2019, 2019, 2020, 2020, 2021]
 
-# data = DataFrame(x = [Date(2018), Date(2019, 2, 14), Date(2019, 9, 17), Date(2020, 2, 13), Date(2020, 6, 11), Date(2021, 10, 11)], 
-#                  y_1 = map(log10, y))
-#
-# ols = lm(@formula(y_1 ~ x), data)
-# print(ols)
+data = DataFrame(y = y_1, x = x)
 
-b=(x'x)\(x'y_1)
+ols = lm(@formula(y ~ x), data)
 
-ŷ=x*b
-
-print(ŷ)
 
 # Plot and save scatterplot
-# gr()
-# plotd = plot(x, y_1, seriestype = :scatter, title = "Scatter plot of Machine Learning models", xlabel = "X_i = Dates()", ylabel = "Y_i = log_10((X_i))")
-# savefig(plotd,"scatterplot_3.png")
+#gr()
+scatter(x, y_1, label="data")
+plot!(x, predict(ols, data), label="model")
+#savefig(plotd,"scatterplot_3.png")
+=#
