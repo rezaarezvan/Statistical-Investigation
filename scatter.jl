34247a2c614bc9cd84d4b6a22464da93db979e91
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2022-10-21 23:50:53 +0200

    Add regression pic and continue questions

--- scatter.jl
+++ scatter.jl
@@ -3,14 +3,12 @@ using Dates
 using GLM
 using DataFrames
 
-function get_beta_coefficent(x::Vector{Date}, y::Vector{Float64})::Float64 
-    Date
-end
-
 df = DataFrame(x = [2018.0, 2019.09, 2019.71, 2020.12, 2020.44, 2021.77])
-y = [0.0936, 1.5, 8.3, 17, 175, 530]
+y  = [0.0936, 1.5, 8.3, 17, 175, 530]
 
 df.y = map(log10, y)
+
 scatter(df.x, df.y, label="Data points")
 model = lm(@formula(y ~ x), df)  # Note how the formula looks exactly like the model you want to build
 plot!(df.x, predict(model, df), label="Linear regression")
+#savefig(plotd,"scatter_regression.png")
